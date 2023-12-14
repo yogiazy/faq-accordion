44b6f4372f7c54b4bf8fea12ba688254fe4a1e75
Yogi Prakoso <yogiarrazy@gmail.com>
2023-12-14 20:54:40 +0700

    update

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ window.onload = function () {
                 n.id = data[i].id;
                 n.classList.add('flex', 'flex-col', 'gap-y-4', 'mt-4');
                 n.innerHTML = `
-                    <div class="flex items-center justify-between gap-x-10">
+                    <div class="flex items-center justify-between gap-x-8">
                         <h2>${data[i].question}</h2>
                         <img src="./assets/images/icon-plus.svg" id="p-${data[i].id}" alt="icon-plus" onclick="toggleFAQ(${data[i].id})">
                         <img src="./assets/images/icon-minus.svg" id="m-${data[i].id}" class="hidden" alt="icon-minus" onclick="toggleFAQ(${data[i].id})">
